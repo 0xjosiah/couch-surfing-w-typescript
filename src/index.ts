ecdd5dc6fe32f422cf44f1f5b7432ac5a43b7d48
Josiah Webb <jj.webb520@gmail.com>
2023-04-18 21:24:51 -0600

    adds images and renders property info

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { displayReviewCount, displayUser } from "./utils"
+const propertyContainer = document.querySelector('.properties') as HTMLElement
 
 const reviews: {
     name: string
@@ -38,5 +39,71 @@ const user: {
     stayedAt: [ 'florida-home', 'oman-flat', 'tokyo-bungalow' ]
 }
 
+// Array of Properties
+const properties : {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+    contact: string;
+    isAvailable: boolean;
+}[] = [
+    {
+        image: 'images/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true  
+    },
+    {
+        image: 'images/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false 
+    },
+    {
+        image: 'images/london-property.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
+    }
+]
+
 displayReviewCount(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 displayUser(user.isReturning, user.userName.first)
+
+// Add the properties
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
+}
--- src/properties.ts
+++ src/properties.ts
@@ -2,23 +2,27 @@ const properties: {
     image: string
     title: string
     price: number
-    street: string
-    city: string
-    zip: number
-    country: string
-    details: string
-    available: boolean
+    location: {
+        street: string
+        city: string
+        zip: number
+        country: string
+    }
+    contactDetails: string
+    isAvailable: boolean
 }[] = [
     {
         image: 'string',
         title: 'string',
         price: 0,
-        street: 'string',
-        city: 'string',
-        zip: 100001,
-        country: 'string',
-        details: 'string',
-        available: false,
+        location: {
+            street: 'string',
+            city: 'string',
+            zip: 100001,
+            country: 'string',
+        },
+        contactDetails: 'string',
+        isAvailable: false,
     },
 ]
 
