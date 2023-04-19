afea14cd5ee8116158be056fd9506468f1cdd776
Josiah Webb <jj.webb520@gmail.com>
2023-04-18 20:51:34 -0600

    creates properties file

---
+++ src/properties.ts
@@ -0,0 +1,25 @@
+const properties: {
+    image: string
+    title: string
+    price: number
+    street: string
+    city: string
+    zip: number
+    country: string
+    details: string
+    available: boolean
+}[] = [
+    {
+        image: 'string',
+        title: 'string',
+        price: 0,
+        street: 'string',
+        city: 'string',
+        zip: 100001,
+        country: 'string',
+        details: 'string',
+        available: false,
+    },
+]
+
+export { properties }
